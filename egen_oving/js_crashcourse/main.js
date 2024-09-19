30f4e426631f67e368f69b6300e3859088f3b9fb
victortonneberg <84105735+victortoneberg@users.noreply.github.com>
2024-09-19 21:21:13 +0200

    Crashcourse

--- egen_oving/js_crashcourse/main.js
+++ egen_oving/js_crashcourse/main.js
@@ -1,3 +1,78 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        //clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Single element
+//console.log(document.getElementById('my-form'));
+//console.log(document.querySelector('h1'));
+
+//Multiple element
+//console.log(document.querySelectorAll('.item'));
+
+const items = document.querySelectorAll('.item')
+items.forEach((item) => console.log(item));
+
+
+const ul = document.querySelector('.items');
+
+// ul.remove();
+//ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+});
+*/
+
+
+
+
+
+
 /* Variabler: let, const
 
 let score;
@@ -112,11 +187,74 @@ const y = 11;
 if(x > 5 && y > 10) {
     console.log('x is more than 5 or y is more than 10');
 } 
-*/
+
 
 //Ternary operator
-const x = 11;
+const x = 9;
 
-const color = x > 10 ? 'red' : 'blue'
+const color = 'green'
 
-console.log(color);
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+        case 'blue':
+        console.log('color is blue');
+        break;
+        default:
+            console.log('color is NOT red or blue');
+}
+
+
+const addNums = num1 => num1 + 5;
+
+
+console.log(addNums(5, 5));
+
+
+//Constructor functions
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+}
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
+}
+
+
+//Class
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+
+//Instantiate
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Victor', 'Smith', '7-4-1981');
+
+//console.log(person1.getBirthYear());
+//console.log(person1.getFullName());
+//console.log(person1);
+//console.log(person2.dob.getFullYear());
+console.log(person2.getFullName());
+console.log(person1);
+
+*/
+
